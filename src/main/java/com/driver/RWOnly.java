3c9bf6465d7ca8566e1a068bdb5d028420c9a60f
ayan <theayan44@gmail.com>
2023-06-09 23:53:34 +0530

    master

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -3,11 +3,11 @@ package com.driver;
 public class RWOnly {
     private String name;
 
-    String getName(){
+    public String getName(){
         return name;
     }
 
-    void setName(String name){
+    public void setName(String name){
         this.name = name;
     }
 }
