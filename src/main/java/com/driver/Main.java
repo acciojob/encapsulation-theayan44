fce140022d6115285f56d7c557a6fd5a591360c3
ayan <theayan44@gmail.com>
2023-06-09 23:47:14 +0530

    master

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly obj = new RWOnly();
+//      obj.name = "Ayan";
+//      System.out.println(obj.name);
+      //java: name has private access in com.driver.RWOnly
+      obj.setter("Ayan");
+      System.out.println(obj.getter());
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    String getter(){
+        return name;
+    }
+
+    void setter(String name){
+        this.name = name;
+    }
 }
